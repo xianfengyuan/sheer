f388a2b765e3094f7f1e7c8af423a7bef4a4ef1a
Xianfeng Yuan <xyuan@samsung.com>
2014-10-22 22:27:21 -0700

    home website

--- sheer.js
+++ sheer.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function (req, res) {
-  res.send('Hello, Timothy!');
+  res.send('Hello, Timothy & Garrett!');
 });
 
 var server = app.listen(app.get('port'), function () {
